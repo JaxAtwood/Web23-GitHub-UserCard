35f376ab90eb47fe3fbbb4ec87a4b185e9b0318d
Jackie Atwood <takalaj@verizon.net>
2019-09-06 14:30:39 -0400

    Updating file.. not showing in GitHub??

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,55 +1,70 @@
-/* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
-*/
 
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
+// axios.get("https://api.github.com/users/JaxAtwood")
+// .then(res => {
+//   console.log(res.data)
+//   card.appendChild(createCard(res.data)); 
+// })
+// .catch(err => {
+//   console.log(err);
+// })
 
-   Skip to Step 3.
-*/
+const followersArray = [
+ 'tetondan',
+ 'dustinmyers',
+ 'justsml',
+ 'luishrd',
+ 'bigknell'
+];
 
-/* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
-*/
+followersArray.forEach (user => {
+  axios.get(`https://api.github.com/users/${user}`)
+    .then (data => {
+      const card = createCard(data.data)
+      const cards = document.querySelector('.cards')
+      cards.appendChild(card)
+    })
+  })
 
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
+function createCard(object) {
+  const card = document.createElement("div");
+  const image = document.createElement("img");
+  const cardInfo = document.createElement("div");
+  const name = document.createElement("h3");
+  const handle = document.createElement("p");
+  const location = document.createElement("p");
+  const profile = document.createElement("p");
+  const url = document.createElement("a");
+  const followers = document.createElement("p");
+  const following = document.createElement("p");
+  const bio = document.createElement("p");
 
-const followersArray = [];
+  card.appendChild(image);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(handle);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  profile.appendChild(url);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
 
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
+  card.classList.add("card");
+  cardInfo.classList.add("card-info");
+  name.classList.add("name");
+  handle.classList.add("username");
 
-<div class="card">
-  <img src={image url of user} />
-  <div class="card-info">
-    <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
-    <p>Profile:  
-      <a href={address to users github page}>{address to users github page}</a>
-    </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
-  </div>
-</div>
+  image.setAttribute("src", object.avatar_url);
+  name.textContent = object.name;
+  handle.textContent = object.login;
+  location.textContent = object.location;
+  url.setAttribute("href", object.html_url);
+  url.textContent = (`${object.html_url}`);
+  followers.textContent = (`Followers: ${object.followers}`);
+  following.textContent = (`Following: ${object.following}`);
+  bio.textContent = (`Bio: ${object.bio}`);
+  
+  console.log(profile);
 
-*/
-
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
+  return card;
+}
